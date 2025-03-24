b26cb8aa7b17631053b55ecfea3db8bfb9fcc2a8
Atticuszz <1831768457@qq.com>
2025-03-24 10:03:13 +0800

    chore: remove redundant `check_url()` in config.utils

--- src/sing_box_cli/config/utils.py
+++ src/sing_box_cli/config/utils.py
@@ -33,16 +33,6 @@ def show_diff_config(current_config: str, new_config: str) -> None:
             print(f"[white]{line}[/white]")
 
 
-def check_url(url: str) -> bool:
-    try:
-        response = httpx.head(url)
-        response.raise_for_status()
-        return True
-    except Exception as e:
-        print(f"❌ Invalid URL: {e}")
-        return False
-
-
 def load_json_config(config_file: Path) -> dict[str, Any]:
     try:
         return dict(json.loads(config_file.read_text(encoding="utf-8")))
