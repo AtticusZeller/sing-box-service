66536343d6948f48e0c9e9fb5c4cb2d7528e9c7a
A.J.Zeller <atticus.zeller@pm.me>
2025-03-25 17:47:49 +0800

    fix:  print friendly as `ensure_root()` failed

--- src/sing_box_cli/common.py
+++ src/sing_box_cli/common.py
@@ -24,9 +24,11 @@ def ensure_root() -> None:
     """https://gist.github.com/RDCH106/fdd419ef7dd803932b16056aab1d2300"""
     try:
         if os.geteuid() != 0:  # type: ignore
-            raise SystemError("⚠️ This script must be run as root.")
+            print("⚠️ This script must be run as root.")
+            raise typer.Exit(1)
     except AttributeError:
         import ctypes
 
         if not ctypes.windll.shell32.IsUserAnAdmin():  # type: ignore
-            raise SystemError("⚠️ This script must be run as Administrator.")
+            print("⚠️ This script must be run as Administrator.")
+            raise typer.Exit(1)
