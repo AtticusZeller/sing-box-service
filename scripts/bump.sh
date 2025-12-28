50c581d653d962021d8b7bf1484427057823c7e5
A.J.Zeller <hello@atticux.me>
2025-12-28 14:55:04 +0800

    fix: remote repo name

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # update CHANGELOG.md use GITHUB_REPO ENV as github token
-git-cliff -o -v --github-repo "atticuszeller/sing-box-service"
+git-cliff -o -v --github-repo "atticuszeller/sing-box-cli"
 # bump version and commit with tags
 bump-my-version bump patch
 # push remote
